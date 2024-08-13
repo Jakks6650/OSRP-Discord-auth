6586d4565bcb467ce212545a627cc3c2bbdc5e1f
Jakks <116462461+Jakks6650@users.noreply.github.com>
2024-08-13 11:48:01 -0400

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
 
   const clientId = process.env.DISCORD_CLIENT_ID;
   const clientSecret = process.env.DISCORD_CLIENT_SECRET;
-  const redirectUri = 'https://osrp-discord-auth.vercel.app/api/auth';
+  const redirectUri = 'https://osrp-discord-auth.vercel.app/';
 
   try {
     const tokenResponse = await axios.post('https://discord.com/api/oauth2/token', new URLSearchParams({
